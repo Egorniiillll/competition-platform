52e76d50d4339527e93b5527d3cffc7103668501
Egorniiillll <119012231+Egorniiillll@users.noreply.github.com>
2026-03-26 16:24:56 +0300

    first blank

---
+++ src/main/java/ru/hse/efremov/competition_platform/CompetitionPlatformApplication.java
@@ -0,0 +1,14 @@
+package ru.hse.efremov.competition_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionPlatformApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CompetitionPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hse/efremov/competition_platform/CompetitionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.efremov.competition_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
