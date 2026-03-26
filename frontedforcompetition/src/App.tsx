c504a822dc0571ed23a255eee94c6678f44088f6
Egorniiillll <119012231+Egorniiillll@users.noreply.github.com>
2026-03-26 16:53:18 +0300

    restructure project into backend and frontend folders

---
+++ frontedforcompetition/src/App.tsx
@@ -0,0 +1,17 @@
+
+import Header from "./Header.tsx";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <h1>Соревнование</h1>
+            <h2>Соревнование</h2>
+            <h3>Соревнование</h3>
+            <p>Соревнование</p>
+            <button>Создать</button>
+        </div>
+    )
+}
+
+export default App
---
+++ frontedforcompetition/src/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="/">Главная</a>
+                <a href="/game">Игры</a>
+                <a href="/account">Мой аккаунт</a>
+            </nav>
+
+        </header>
+
+    )
+}
+
+export default Header;
